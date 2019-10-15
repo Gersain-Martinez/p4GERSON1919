a6ad000a7d8ac25bc0b83c4721279d597fcf72b8
Gersian-Martinez <martinezgersain30@gmail.com>
2019-10-15 14:08:24 -0500

    Initial commit

---
+++ app/src/main/java/com/gmh/itics/tesoem/edu/p4gerson1919/MainActivity.java
@@ -0,0 +1,26 @@
+package com.gmh.itics.tesoem.edu.p4gerson1919;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText vnombre;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        vnombre= (EditText) findViewById(R.id.txtNombre);
+    }
+    public void Invocar(View v){
+        Intent pantalla = new Intent(this,RecibeParametrosActivity.class);
+        pantalla.putExtra( "nombre", vnombre.getText().toString());
+        startActivity(pantalla);
+        this.finish();
+
+    }
+}
---
+++ app/src/main/java/com/gmh/itics/tesoem/edu/p4gerson1919/RecibeParametrosActivity.java
@@ -0,0 +1,21 @@
+package com.gmh.itics.tesoem.edu.p4gerson1919;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class RecibeParametrosActivity extends AppCompatActivity {
+    TextView vnombre;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recibe_parametros);
+        vnombre=(TextView)findViewById(R.id.lblNombre);
+        Bundle paramatetros = getIntent().getExtras();
+        vnombre.setText("Bienvenido"+ paramatetros.getString("nombre"));
+    }
+}
